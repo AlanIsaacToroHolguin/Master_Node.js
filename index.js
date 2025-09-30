07eac393ae470ba0af92d360771ee010aac6146c
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-09-30 11:27:11 -0500

    Testing First Routes

--- index.js
+++ index.js
@@ -25,9 +25,55 @@ app.use(cors());
 // Middleware: automatically parse incoming request bodies to JSON
 app.use(express.json());
 
-// TODO: Here you can define your routes (e.g., app.get, app.post, etc.)
+// Routes: Here you can define your routes (e.g., app.get, app.post, etc.)
+// Example 1: Returning plain HTML as a response
+/*app.get("/probando", (req, res) => {
+    console.log("The endpoint was executed correctly");
+
+    // res.send() can be used to send text or HTML
+    return res.status(200).send(`
+        <h1>Hello world from NodeJS and Express</h1>
+    `);
+});
+
+
+// Example 2: Returning a JavaScript object with res.send()
+// Express will automatically convert the object into JSON
+app.get("/probando", (req, res) => {
+    console.log("The endpoint was executed correctly");
+
+    return res.status(200).send({
+        Book: "Midnight Sun",
+        Author: "Alan Toro",
+        URL: "soldemedianoche.com"
+    });
+});
+*/
+
+// Example 3: Returning JSON explicitly using res.json()
+// This is the most common way when building APIs
+app.get("/probando", (req, res) => {
+    console.log("The endpoint was executed correctly");
+
+    return res.status(200).json([{
+        Book: "Midnight Sun",
+        Author: "Alan Toro",
+        URL: "soldemedianoche.com"
+    },
+    {
+        Book: "Be challenger in lol",
+        Author: "Jarod Chavez",
+        URL: "soldemedianoche.com"
+    },
+]);
+});
+
+
+
+
 
 // Start the server and listen for HTTP requests
 app.listen(port, () => {
     console.log("Server running on port " + port);
 });
+
